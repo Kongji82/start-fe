28f2f5bc684a0747732d8e08353351e6acc566a2
kongji <kongji4092@gmail.com>
2023-03-23 17:25:17 +0900

    4주차

---
+++ 04/quiz.js
@@ -0,0 +1,101 @@
+// // Quiz 1
+// const year = 2022;
+// const month = 12;
+// const day = 3;
+// let dateString = '';
+
+// if (day < 10){
+//     dateString = `${year}-${month}-0${day}`
+// } else {
+//     dateString = `${year}-${month}-${day}`
+// }
+
+// console.log(dateString);
+
+// // Quiz 2
+// const dateString2 = "2022-12-12";
+// let result2 = dateString2.split('-').map(str => parseInt(str));
+// console.log(result2)
+
+// // Quiz 3
+// const text = "나는 XX를 좋아해";
+// const item = "JS";
+// const result3 = text.replace('XX', item);
+
+// console.log(result3);
+
+// // Quiz 4
+// const text4 = "안녕하세요. 나는 사람입니다.";
+// const searchText = "사람";
+
+// if (text4.indexOf(searchText) > 0){
+//     console.log('false');
+// } else {
+//     console.log('true');
+// }
+
+// // Quiz 5
+// const point1 = [12, 34, 80, 72];
+// const point2 = [1, 80, 94];
+
+// const sum = point1.concat(point2).reduce((a, b) => (a+b))
+// console.log(`Quiz 5: ${sum}`)
+
+// Quiz 6
+// const a = 1.2;
+// const b = 3.4;
+
+// console.log(Math.floor(a) + Math.floor(b));
+
+// Quiz 7
+// const min = 1;
+// const max = 10;
+// const randomNumber = Math.floor(Math.random() * max) + min;
+
+// console.log(randomNumber)
+
+// Quiz 8
+// const target = 10;
+// const num = 3;
+
+// if (target % num == 0) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+// Quiz 9
+// const nick = "kakao";
+// console.log(nick.toUpperCase());
+
+// Quiz 10
+// const items = [1, "a", true, ["a"], { n: 1 }];
+// let result10 = []
+// for (i=0; i < items.length; i++){
+//     if (typeof(items[i]) != "object"){
+//         result10.push(items[i])
+//     }
+// }
+// console.log(result10)
+
+// Quiz 11
+
+
+
+// Quiz 12
+
+// Quiz 13
+
+// Quiz 14
+
+// Quiz 15
+
+// Quiz 16
+
+// Quiz 17
+
+// Quiz 18
+
+// Quiz 19
+
+// Quiz 20
---
+++ 04/test.js
@@ -0,0 +1,24 @@
+// const title = 'jeju';
+// const url = 'https://daum.net.\
+//     asd\
+// '
+// const url2 = `https://daum.net/${title}`
+
+// console.log(url, url2);
+
+// function sum(x, y=0){
+//     return x + y;
+// }
+
+// console.log(sum(10));
+
+// const result = [1, 2, 3].map((item) => {
+//     return item + 1;
+// })
+
+// console.log(result)
+
+// const number = 1;
+// const obj = {
+//     number: number,
+// };
