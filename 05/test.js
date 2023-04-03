9bdda7ef4be71e9d8fbefa60edc9978c1bbd5054
kongji <kongji4092@gmail.com>
2023-04-03 22:18:31 +0900

    quiz1

---
+++ 05/test.js
@@ -0,0 +1,9 @@
+console.log('05')
+
+const element = document.getElementById('wrap')
+console.log(wrap)
+const item = document.querySelector('.item');
+console.log(item);
+
+const elements = document.querySelector('div');
+console.log(elements)
