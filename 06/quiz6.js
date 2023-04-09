d9ce11e9ed4eb23974b2910805a37e44d45dc2b9
kongji <kongji4092@gmail.com>
2023-04-09 20:44:45 +0900

    quiz6

---
+++ 06/quiz6.js
@@ -0,0 +1,24 @@
+const $num = document.querySelector('#num');
+const $button = document.querySelector('#btn');
+const $result = document.querySelector('#result');
+
+$button.addEventListener('click', () => {
+    let value = parseInt($num.value);
+    let resultHtml = '';
+    if (isNaN(value)){
+        alert("숫자를 입력해주세요");
+    } else {
+        for (let i = 1; i < 10; i++) {
+            resultHtml += `${value} x ${i} = ${value * i}<br>`;
+        }
+        $result.innerHTML = resultHtml;
+    }
+    
+});
+
+
+function init(){
+    
+}
+
+init()
