37dac0df7c16ec284cf9d9b6ac728a8288ae6462
kongji <kongji4092@gmail.com>
2023-04-19 10:45:32 +0900

    login form 리팩토링

--- 07/login.js
+++ 07/login.js
@@ -1,26 +1,19 @@
 // 입력한 id,pw가 맞을경우엔 success , 아니면 error출력
 
-const $btn = document.querySelector('#btn');
-const $id = document.querySelector('.id');
-const $pw = document.querySelector('.pw');
 const $result = document.querySelector('#result');
+const $form = document.querySelector('form');
 
 const id = 'hello';
 const pw = 'world';
 
-$btn.addEventListener('click', (e) => {
+$form.addEventListener('submit', (e) => {
   e.preventDefault();
-  if (id === $id.value && pw === $pw.value) {
-    const successMsg = document.createElement('div');
-    successMsg.classList.add('success');
-    successMsg.textContent = 'Success';
-    result.innerHTML = '';
-    result.appendChild(successMsg);
-  } else {
-    const errorMsg = document.createElement('div');
-    errorMsg.classList.add('error');
-    errorMsg.textContent = 'Error';
-    result.innerHTML = '';
-    result.appendChild(errorMsg);
-  }
+  const $id = $form.elements.id.value;
+  const $pw = $form.elements.pw.value;
+  const msg = document.createElement('div');
+  let judge = id === $id && pw === $pw ? 'success' : 'error';
+  msg.classList.add(judge);
+  msg.textContent = judge;
+  $result.innerHTML = '';
+  $result.appendChild(msg);
 });
